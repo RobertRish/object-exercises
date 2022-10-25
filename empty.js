14495160b630d3847df7695a18979ea26c65ea61
RobertRish <robert.rish1225@gmail.com>
2022-10-25 00:55:38 -0400

    complete exercises

---
+++ empty.js
@@ -0,0 +1,9 @@
+// Write the function isEmpty(obj) which returns true if the object has no 
+// properties, false otherwise.
+
+function isEmpty(obj) {
+    for (let key in obj) {
+        return false;
+    }
+    return true;
+}
